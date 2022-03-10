9292ad300c991975cfba4c9a5926e80607e6a032
jgarz800 <jgarz800@stu.southtexascollege.edu>
2022-03-09 22:07:11 -0600

    Secret: Replaced Lorem Ipsum with a Video

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,6 +1,13 @@
 <template>
-    <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis dolorum quibusdam quo natus eos corporis non, voluptatem iste dolore itaque laudantium illo, ratione maxime delectus nesciunt. Et ipsum enim quidem?
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos facere quas unde laborum voluptatibus illo amet odit fugit delectus! Nostrum, vel nobis dicta culpa ducimus quaerat impedit at voluptatem officia.
-    </p>
+
+    <div class="flex items-center justify-center h-screen-nonav">
+        <iframe 
+        width="890" 
+        height="560" 
+        src="https://www.youtube.com/embed/Lcw4Xlp0AhI" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen></iframe>
+    </div>
 </template>
