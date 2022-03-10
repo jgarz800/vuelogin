05b5c94283654177665fe84fd936837c10bdc762
jgarz800 <jgarz800@stu.southtexascollege.edu>
2022-03-09 21:21:43 -0600

    Regiseter Done

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { findProp } from "@vue/compiler-core";
+//import { findProp } from "@vue/compiler-core";
 import {ref} from "vue";
 import { useRouter } from "vue-router";
 import useAuth from "../composable/useAuth";
 import useError from "../composable/useError";
 import { useTimeout, promiseTimeout} from '@vueuse/core';
 
-const {isAuthenticated, login} = useAuth();
+const {isAuthenticated, login, signup} = useAuth();
 
 const username = ref("");
 const password = ref("");
@@ -15,12 +15,21 @@ const router = useRouter();
 
 const logginIn = async () => {
     await login(username.value, password.value);
+     goToHome();  
+}
+
+const signingUp = async () => {
+ await signup(username.value, password.value);
+ goToHome();  
+}
+
+const goToHome = () => {
     if(isAuthenticated.value){
         router.push("/");
     } else{
         setError("Invalid username or password")
         start();
-    }
+    };
 }
 
 const {error, setError} = useError();
@@ -38,7 +47,14 @@ const { ready, start } = useTimeout(3000, { controls: true }, true)
             <!--input with label-->
             <input type="text" class="p-2 border-2 rounded-lg" placeholder="Username" v-model="username">
             <input type="password" class="p-2 border-2 rounded-lg" placeholder="Password" v-model="password">
-            <button type="submit" @submit.prevent="logginIn" class="py-2 text-indigo-200 bg-indigo-600 rounded-lg">Login</button>
+            <div class="flex space-x-2">
+                <button type="submit" @submit.prevent="logginIn" class="w-1/2 py-2 text-red-200 bg-red-600 rounded-lg">Login</button>
+            <button  
+            @click="signingUp" 
+            class="w-1/2 py-2 text-orange-200 bg-orange-600 rounded-lg">
+            Sign Up</button>
+                        </div>
+
         </form>
     </div>
     <div
