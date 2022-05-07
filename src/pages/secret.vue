852fa712e3e45bd3fb58a6073da2647786b81a47
jgarz800 <jgarz800@stu.southtexascollege.edu>
2022-05-06 22:35:10 -0500

    Send message done

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,10 +1,17 @@
 <script setup>
     import { onUnmounted, ref } from 'vue'
     import useChat from '../composable/useChat'
+    import useAuth from '../composable/useAuth';
 
-    const {messages, unsubscribe} = useChat()
+    const {messages, unsubscribe, sendMessage} = useChat()
+    const {user} = useAuth()
 
     const newMessage = ref('')
+
+    const send = () => {
+        sendMessage(newMessage.value)
+        newMessage.value = ''
+    }
 
     onUnmounted(() => {
         unsubscribe
@@ -28,7 +35,8 @@
   >
     <ul class="p-4 space-y-4">
       <li v-for="message in messages" :key="message.id">
-        <div class="flex justify-between px-4 py-2 bg-gray-200 rounded-lg">
+        <div class="flex justify-between px-4 py-2 rounded-lg" 
+        :class="user === message.author ? 'bg-purple-200' : 'bg-blue-200'">
           <span>{{message.text}}</span>
           <span>by {{message.author}}</span>
         </div>
@@ -40,6 +48,7 @@
         type="text"
         placeholder="Type a message..."
         v-model="newMessage"
+        @change="send"
       />
     </div>
   </div>
